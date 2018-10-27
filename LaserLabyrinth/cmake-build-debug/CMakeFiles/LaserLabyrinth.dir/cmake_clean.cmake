0a3754558c3550fba0939a3617734f40770e04df
Трунников Максим Владиславович <maxonfjvipon19@yandex.ru>
2018-10-27 13:01:51 +0300

    First commit

---
+++ LaserLabyrinth/cmake-build-debug/CMakeFiles/LaserLabyrinth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LaserLabyrinth.dir/main.cpp.o"
+  "LaserLabyrinth.pdb"
+  "LaserLabyrinth"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LaserLabyrinth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
